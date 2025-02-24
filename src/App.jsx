744709eb6d957e306ea3f0e71493b258a567d7dd
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-02-25 01:25:09 +0800

    changes on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import AdminDashboard from "./components/Admin/AdminDashboard";
+import AdminDashboard from "./components/Admin/dashboard/AdminDashboard";
 import ProtectedRoute from "./routes/protectedRoute";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './styles/output.css'
 import Login from './components/login'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTachometerAlt, faListAlt, faWarehouse, faTools, faExclamationTriangle, faRecycle, faUsers, faUserTie, faUser, faBars, faAngleDown} from "@fortawesome/free-solid-svg-icons"
+
+library.add(faTachometerAlt, faListAlt, faWarehouse, faTools, faExclamationTriangle, faRecycle, faUsers, faUserTie, faUser, faBars, faAngleDown)
 
   function App() {
     return (
