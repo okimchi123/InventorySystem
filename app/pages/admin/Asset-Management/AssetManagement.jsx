a044734b28fe999fb8c609aeac9f933161150771
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-03-12 15:24:35 +0800

    added topstats cards

--- app/pages/admin/Asset-Management/AssetManagement.jsx
+++ app/pages/admin/Asset-Management/AssetManagement.jsx
@@ -1,10 +1,12 @@
 import AssetTable from "../../../components/Admin/Asset-Management/AssetTable";
+import TopStat from "../../../components/Admin/Asset-Management/TopStat";
 
 export default function AssetManagement() {
   return (
     <>
       <div class="pt-22 px-10 py-6 laptop:px-12 phone:px-4">
         {/* <!-- contents --> */}
+        <TopStat />
         <AssetTable />
       </div>
     </>
