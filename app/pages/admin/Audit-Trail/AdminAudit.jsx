5f619d98d1e53d8ee50cda26b68675a3878da40d
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-03-18 10:32:02 +0800

    search function audit page

--- app/pages/admin/Audit-Trail/AdminAudit.jsx
+++ app/pages/admin/Audit-Trail/AdminAudit.jsx
@@ -4,6 +4,8 @@ import moment from "moment";
 
 export default function AuditTrail() {
   const [assetLogs, setAssetLogs] = useState([]);
+  const [filteredLogs, setFilteredLogs] = useState(assetLogs);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const fetchLogs = useCallback(async () => {
     try {
@@ -22,7 +24,15 @@ export default function AuditTrail() {
 
   useEffect(() => {
     fetchLogs();
-  });
+  }, []); 
+  
+  useEffect(() => {
+    const filtered = assetLogs.filter((log) =>
+      log.userEmail.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      log.productName.some((name) => name.toLowerCase().includes(searchQuery.toLowerCase()))
+    );
+    setFilteredLogs(filtered);
+  }, [searchQuery, assetLogs]);
 
   return (
     <div className="pt-22 py-6 laptop:px-12 px-10 phone:px-4">
@@ -31,38 +41,74 @@ export default function AuditTrail() {
           <h1 className="text-2xl font-bold">Asset History</h1>
           <input
             type="text"
-            placeholder="Search User"
+            placeholder="Search email or product name"
             className="h-10 py-4 px-3 border border-gray-700 shadow-sm sm:text-md outline-none rounded-2xl"
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
         </div>
-        <div class="w-full overflow-x-auto h-full border border-gray-300 rounded-lg shadow-md">
-          <table class="w-full bg-white">
+        <div className="w-full overflow-x-auto h-full border border-gray-300 rounded-lg shadow-md">
+          <table className="w-full bg-white">
             <thead>
-              <tr class="bg-gray-200 border-gray-400 text-md text-left px-4">
-                <th class="py-3 px-4 whitespace-nowrap">Date</th>
-                <th class="py-3 px-4 whitespace-nowrap">User</th>
-                <th class="py-3 px-4 whitespace-nowrap">Role</th>
-                <th class="py-3 px-4 whitespace-nowrap">Product</th>
-                <th class="py-3 px-4 whitespace-nowrap">
+              <tr className="bg-gray-200 border-gray-400 text-md text-left px-4">
+                <th className="py-3 px-4 whitespace-nowrap">Date</th>
+                <th className="py-3 px-4 whitespace-nowrap">User</th>
+                <th className="py-3 px-4 whitespace-nowrap">Role</th>
+                <th className="py-3 px-4 whitespace-nowrap">Product</th>
+                <th className="py-3 px-4 whitespace-nowrap">
                   Product Serial Number
                 </th>
-                <th class="py-3 px-4 whitespace-nowrap">Action</th>
+                <th className="py-3 px-4 whitespace-nowrap">Action</th>
               </tr>
             </thead>
             <tbody>
-              {assetLogs.map((log) => (
+              {filteredLogs.map((log) => (
                 <tr
                   key={log._id}
                   className="text-left border-gray-300 border-b-[1px]"
                 >
-                  <td class="py-4 px-4 whitespace-nowrap">
+                  <td className="py-4 px-4 whitespace-nowrap">
                     {moment(log.createdAt).format("MMMM D, YYYY h:mm A")}
                   </td>
-                  <td class="py-4 px-4 whitespace-nowrap">{log.userEmail}</td>
-                  <td class="py-4 px-4 whitespace-nowrap">{log.userRole}</td>
-                  <td class="py-4 px-4 whitespace-nowrap">{log.productName}</td>
-                  <td class="py-4 px-4 whitespace-nowrap">{log.productSN}</td>
-                  <td class="py-4 px-4 whitespace-nowrap">{log.action}</td>
+                  <td className="py-4 px-4 whitespace-nowrap">{log.userEmail}</td>
+                  <td className="py-4 px-4 whitespace-nowrap">{log.userRole}</td>
+                  <td className="py-4 px-4">
+                    <div className="w-[180px] break-words whitespace-normal">
+                      {log.productName.length > 1
+                        ? log.productName.map((name, index) => (
+                            <span key={index}>
+                              {name}
+                              {index !== log.productName.length - 1 && ", "}
+                            </span>
+                          ))
+                        : log.productName}
+                    </div>
+                  </td>
+                  <td className="py-4 px-4">
+                    <div className="w-[200px] break-words whitespace-normal">
+                      {log.productSN.length > 1
+                        ? log.productSN.map((name, index) => (
+                            <span key={index} className="">
+                              {name}
+                              {index !== log.productSN.length - 1 && ", "}
+                            </span>
+                          ))
+                        : log.productSN}
+                    </div>
+                  </td>
+                  <td className="py-4 px-4 whitespace-nowrap">
+                    <div
+                      className={`py-[6px] rounded-lg text-center text-[18px] 
+                      ${
+                        log.action === "CREATE"
+                          ? "text-green-900 bg-green-100"
+                          : log.action === "UPDATE"
+                          ? "text-yellow-900 bg-yellow-100"
+                          : "text-red-900 bg-red-200"
+                      }`}
+                    >
+                      <span>{log.action}</span>
+                    </div>
+                  </td>
                 </tr>
               ))}
             </tbody>
