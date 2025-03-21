b4612e3ddca13372618a77514173e172bef361a2
Okimchi <151455386+okimchi123@users.noreply.github.com>
2025-03-21 16:30:54 +0800

    fix frontend

--- app/components/Admin/User-Management/userTable.jsx
+++ app/components/Admin/User-Management/userTable.jsx
@@ -264,7 +264,7 @@ export default function UserTable({ openModal }) {
                 <td className="py-4 px-4 whitespace-nowrap">{`${user.firstname} ${user.lastname}`}</td>
                 <td className="py-4 px-4 whitespace-nowrap">{user.role}</td>
                 <td className="py-4 px-4 whitespace-nowrap">{user.email}</td>
-                <td className="py-4 px-4 whitespace-nowrap">{user.phone}</td>
+                <td className="py-4 px-4 whitespace-nowrap">{user.phone?.toString().padStart(11, "0")}</td>
                 <td className="py-4 px-4 whitespace-nowrap">
                   <div
                     className={`w-[75px] py-[6px] rounded-lg text-center text-[18px]
