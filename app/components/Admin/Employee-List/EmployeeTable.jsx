14fdbebcb91259b8f568c0330a2eab3032a38035
Okimchi <basilio.miko123@gmail.com>
2025-03-22 16:34:17 +0800

    added employee table and view modal

--- app/components/Admin/Employee-List/EmployeeTable.jsx
+++ app/components/Admin/Employee-List/EmployeeTable.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ViewAsset from "./ViewAsset";
+import AssetTable from "../Asset-Management/AssetTable";
 
 const ITEMS_PER_PAGE = 8;
 
 export default function EmployeeTable() {
   const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  function handleViewAssets(user) {
+    setSelectedUser(user);
+    setModalOpen(true);
+  }
 
   const fetchUsers = useCallback(async () => {
     try {
@@ -29,6 +38,12 @@ export default function EmployeeTable() {
 
   return (
     <div className="flex flex-col items-end justify-center w-full mx-auto">
+      <ViewAsset
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        user={selectedUser}
+      />
+
       <div className="flex gap-3 mx-auto py-4 w-full">
         <input
           type="text"
@@ -48,27 +63,35 @@ export default function EmployeeTable() {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
-              <tr
-                key={user._id}
-                className="text-left border-gray-300 border-b-[1px]"
-              >
-                <td className="py-4 px-4 whitespace-nowrap">{user.email}</td>
-                <td className="py-4 px-4 whitespace-nowrap">
-                  {user.firstname} {user.lastname}
-                </td>
-                <td className="py-4 px-4">{user.role}</td>
-                <td className="py-4 px-4">
-                  {user.phone?.toString().padStart(11, "0")}
-                </td>
-                <td className="py-4 px-0 whitespace-nowrap">
-                  <button className="text-[18px] bg-blue-900 text-white py-2 px-4 rounded-2xl cursor-pointer font-semibold hover:bg-blue-700 transition-all">
-                    <FontAwesomeIcon icon="up-right-and-down-left-from-center" className="mr-2.5"/>
-                    view assets
-                  </button>
-                </td>
-              </tr>
-            ))}
+            {users
+              .filter((user) => user.handlingAssets != 0)
+              .map((user) => (
+                <tr
+                  key={user._id}
+                  className="text-left border-gray-300 border-b-[1px]"
+                >
+                  <td className="py-4 px-4 whitespace-nowrap">{user.email}</td>
+                  <td className="py-4 px-4 whitespace-nowrap">
+                    {user.firstname} {user.lastname}
+                  </td>
+                  <td className="py-4 px-4">{user.role}</td>
+                  <td className="py-4 px-4">
+                    {user.phone?.toString().padStart(11, "0")}
+                  </td>
+                  <td className="py-4 px-0 whitespace-nowrap">
+                    <button
+                      onClick={() => handleViewAssets(user)}
+                      className="text-[18px] bg-blue-900 text-white py-2 px-4 rounded-2xl cursor-pointer font-semibold hover:bg-blue-700 transition-all"
+                    >
+                      <FontAwesomeIcon
+                        icon="up-right-and-down-left-from-center"
+                        className="mr-2.5"
+                      />
+                      view assets
+                    </button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
