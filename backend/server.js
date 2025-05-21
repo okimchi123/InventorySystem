278ba730a2fa2f79a6e5476f2a9b145066590907
Okimchi <basilio.miko123@gmail.com>
2025-05-22 01:08:07 +0800

    server

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173", 
-      "https://express-inventory-system.vercel.app/" 
+      "https://express-inventory-system.vercel.app" 
     ],
     methods: ["GET", "POST"],
     credentials: true
